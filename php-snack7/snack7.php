f458ae55272360bc6e8ed9a76ed17554a18173f1
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-06-07 18:07:01 +0200

    created array with students and grades

--- php-snack7/snack7.php
+++ php-snack7/snack7.php
@@ -1,3 +1,54 @@
+<?php
+
+$arrStudenti = [
+    [
+        "nome" => "Mario",
+        "cognome" => "Rossi",
+        "voti" => [
+            "matematica" => 6,
+            "italiano" => 5,
+            "fisica" => 8,
+            "storia" => 2
+        ]
+    ],
+    [
+        "nome" => "Giulio",
+        "cognome" => "Verdi",
+        "voti" => [
+            "matematica" => 5,
+            "italiano" => 7,
+            "fisica" => 3,
+            "storia" => 9
+        ]
+    ],
+    [
+        "nome" => "Marco",
+        "cognome" => "Polo",
+        "voti" => [
+            "matematica" => 4,
+            "italiano" => 6,
+            "fisica" => 7,
+            "storia" => 6
+        ]
+    ],
+    [
+        "nome" => "Paolo",
+        "cognome" => "Brosio",
+        "voti" => [
+            "matematica" => 7,
+            "italiano" => 6,
+            "fisica" => 9,
+            "storia" => 6
+        ]
+    ],
+    
+];
+
+var_dump($arrStudenti);
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
