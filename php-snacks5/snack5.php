c8d561557df488dcc9227a8a6578a9e63a72b277
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-06-07 16:47:10 +0200

    printed in page each paragraph divided by "."

--- php-snacks5/snack5.php
+++ php-snacks5/snack5.php
@@ -3,8 +3,6 @@
 $paragraphsample = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, eligendi necessitatibus, laudantium maxime rerum vero consequatur quos natus doloremque mollitia dolorem! Nemo dolor laudantium natus impedit alias aut quidem vitae magnam numquam mollitia adipisci neque eligendi recusandae est fugiat quibusdam expedita a officia qui, repellendus doloribus quasi eius dolorem autem. Voluptatem voluptatibus sequi dolores nulla delectus error iste iusto quibusdam, reiciendis beatae exercitationem suscipit. Consequuntur magni vero magnam ipsum recusandae, qui iure doloremque nihil labore architecto. Odio magni quo ratione quod dolores minus expedita dolore neque asperiores reprehenderit tempore impedit, sequi commodi labore fuga temporibus voluptate quis sunt eum, est modi eaque sit quisquam! Alias obcaecati numquam voluptatibus facilis nobis laborum provident quibusdam accusantium dolorem, perspiciatis maxime sint maiores quo eligendi veritatis nam doloribus explicabo consequatur? Culpa non cupiditate enim! Odio, impedit. Perferendis, omnis dolorem quisquam quibusdam dolores enim veniam earum tempora illum sit vitae dolorum pariatur iste quam, delectus minima facilis optio natus culpa fugiat consectetur! Quisquam natus labore animi illum, voluptatibus accusamus consequuntur veritatis placeat necessitatibus quasi incidunt eos non saepe amet porro illo beatae culpa odit, explicabo reiciendis et facilis. Minus laborum magnam numquam exercitationem deleniti ea nihil unde recusandae veritatis aliquid ipsum fugiat odio nemo mollitia culpa distinctio, reiciendis magni iure dolorum voluptate minima dignissimos, consequuntur earum? Eligendi totam nulla molestiae molestias, nostrum expedita, doloribus odit incidunt eaque cum numquam facere labore repellat omnis pariatur quia id blanditiis a minima et nemo laborum? Adipisci est officia placeat fugit soluta facilis delectus. Enim, quidem cumque adipisci voluptas dolorem inventore sed eveniet consequuntur fugit rerum excepturi doloribus at. Animi debitis minima odit sapiente quis! Sed molestias debitis veniam cupiditate, non odit saepe doloribus beatae, iste unde id nobis omnis ipsa. Doloremque tempora assumenda voluptate aliquid, fuga, recusandae, beatae eos at qui accusantium ab. Amet fugiat tempore numquam iusto modi. Alias consequatur illo autem velit laboriosam fuga natus iste odio, deserunt, fugit labore tenetur veniam iusto vitae, ducimus exercitationem aliquam atque! Assumenda consectetur possimus eveniet error? Optio eligendi repudiandae facere exercitationem. Eveniet, excepturi? Expedita, optio fuga adipisci dignissimos, odio, explicabo repudiandae mollitia omnis nisi molestiae vitae? Iure aliquam rerum ab qui veniam! Maiores atque fuga aut nam. Aspernatur officia velit ullam repellat, delectus perferendis sed ab omnis id, animi ipsam voluptatum, ea at nulla eligendi sapiente maiores quod. Nisi, fugiat! Dolore, rerum alias. Sed, aliquam! Non, repellat quam neque velit aperiam molestiae corporis ad cumque deleniti similique, provident amet exercitationem iste? Dolor maiores, esse placeat nisi porro recusandae corporis similique dicta expedita tempora sunt earum amet. Pariatur recusandae alias eligendi. Quidem voluptatem, voluptatibus tempore cupiditate fugiat veniam aut error consequatur placeat maxime possimus? Explicabo laboriosam, voluptatum repellat odit expedita unde eaque obcaecati vero libero debitis sit, in, aspernatur architecto illum officiis nostrum voluptatibus ullam rerum quis quasi accusamus. Aliquam corporis voluptates, sunt dolorem repellendus quas ratione atque, minus ab, ducimus totam optio? Laudantium corporis illum dolor aspernatur fuga voluptas consequuntur molestiae, dolores hic consequatur accusantium dignissimos eligendi molestias unde aut reprehenderit nemo natus. Saepe quidem totam inventore tempore veritatis.";
 
 $singleparagraphs = explode(".", $paragraphsample);
-
-var_dump($singleparagraphs);
 ?>
 
 <!DOCTYPE html>
@@ -18,6 +16,15 @@ var_dump($singleparagraphs);
 <body>
     <h1>SNACK 5</h1>
 
+    <p>
+        <?php
+        
+        for($i = 0; $i < count($singleparagraphs); $i++){
+            echo "<p>$singleparagraphs[$i]</p><hr>";
+        }
+
+        ?>
+    </p>
 
 </body>
 </html>
