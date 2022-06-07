8020a3905ed569ef9053303f67fc6fdd3d4a99a7
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-06-07 16:38:35 +0200

    snack 2 working

--- php-snacks/snack2.php
+++ php-snacks/snack2.php
@@ -4,6 +4,11 @@ $name = $_GET["name"];
 $mail = $_GET["mail"];
 $age = $_GET["age"];
 
+$chiocciola = "@";
+$punto = ".";
+
+$logged = "Accesso riuscito";
+$blocked = "Accesso negato";
 ?>
 
 
@@ -17,5 +22,19 @@ $age = $_GET["age"];
 </head>
 <body>
     <h1>SNACK 2</h1>
+
+    <?php
+    if(empty($name) || empty($mail) || empty($age)){
+        echo $blocked;
+    }elseif(strlen($name) < 3){
+        echo $blocked;
+    }elseif(!strpos($mail, $chiocciola) || !strpos($mail, $punto)){
+        echo $blocked;
+    }elseif(!is_numeric($age)){
+        echo $blocked;
+    }else{
+        echo $logged;
+    }
+    ?>
 </body>
 </html>
